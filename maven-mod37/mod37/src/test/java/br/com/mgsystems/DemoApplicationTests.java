fb13b7a52d0038bdb018fbfa13509c3e8680d484
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2024-04-08 13:15:57 -0400

    commit

---
+++ maven-mod37/mod37/src/test/java/br/com/mgsystems/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mgsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
